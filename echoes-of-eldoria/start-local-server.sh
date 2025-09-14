ca5194a8d119ce2a9c10dbed8cc8da76c9bcbd92
Sameer30220 <soc30220@gmail.com>
2025-09-14 21:46:24 +0530

    Add files via upload

---
+++ echoes-of-eldoria/start-local-server.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+# # This script starts a simple HTTP server on port 5500 using Python
+
+# # Check if python3 is installed
+# if ! command -v python3 &> /dev/null
+# then
+#     echo "python3 could not be found. Please install Python 3 to run the local server."
+#     exit 1
+# fi
+
+# # echo "Starting local server at http://localhost:5500"
+# python3 -m http.server 5500
